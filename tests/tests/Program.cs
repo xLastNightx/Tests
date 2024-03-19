58d02e33a1bd722594890527a13d7ae73756c817
Kate <mikhalevich%katya.com>
2024-03-19 12:50:33 +0300

    program.cs изменила содержимое

--- tests/tests/Program.cs
+++ tests/tests/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("HellYes");
+﻿Console.Write("Введите имя: ");
+string? name = Console.ReadLine(); // ? означает, что может быть значение NULL
+
+Console.Write("Введите возраст: ");
+int age = Convert.ToInt32(Console.ReadLine()); // переводит из string в int
+
+Console.Write("Введите рост: ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите размер зарплаты: ");
+decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
