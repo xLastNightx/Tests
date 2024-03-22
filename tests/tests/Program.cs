1455c02a783825451d93a21b9993f2394201c59f
Kate <mikhalevich%katya.com>
2024-03-22 10:30:49 +0300

    +-*/

--- tests/tests/Program.cs
+++ tests/tests/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите имя: ");
+﻿/*Console.Write("Введите имя: ");
 string? name = Console.ReadLine(); // ? означает, что может быть значение NULL
 
 Console.Write("Введите возраст: ");
@@ -10,4 +10,39 @@ double height = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите размер зарплаты: ");
 decimal salary = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
+Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$"); */
+
+int a, b, c;
+
+Console.WriteLine("Выберите \n sum, diff, product, quotient");
+string? toDo = Console.ReadLine();
+if (toDo == "")
+{
+    Console.WriteLine("неверное условие");
+} 
+else
+{
+    Console.WriteLine("Введите значения");
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    if (toDo == "sum")
+    {
+        c = a + b;
+        Console.WriteLine($"Ответ: {c}");
+    }
+    if (toDo == "diff")
+    {
+        c = a - b;
+        Console.WriteLine($"Ответ: {c}");
+    }
+    if (toDo == "product")
+    {
+        c = a * b;
+        Console.WriteLine($"Ответ: {c}");
+    }
+    if (toDo == "quotient")
+    {
+        c = a / b;
+        Console.WriteLine($"Ответ: {c}");
+    }
+}
