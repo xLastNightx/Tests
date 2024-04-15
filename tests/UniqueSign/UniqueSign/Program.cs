61b250fc39b7346e657287206997ae5826f6f0f3
Kate <mikhalevich%katya.com>
2024-04-15 12:58:30 +0300

    unique signs

---
+++ tests/UniqueSign/UniqueSign/Program.cs
@@ -0,0 +1,21 @@
+﻿string str = "hello wold";
+int count = 0;
+int tmp;
+Console.WriteLine(str.Length);
+for (int i = 0; i < str.Length; i++)
+{
+    tmp = 0;
+    for (int j = 0; j < str.Length; j++)
+    {
+        if (str[i] == str[j])
+        {
+            tmp++;
+        }
+    }
+    if (tmp == 1)
+    {
+        Console.WriteLine($"Уникальный символ {str[i]}");
+        count++;
+    }
+}
+Console.WriteLine($"Всего {count} уникальных символов");
