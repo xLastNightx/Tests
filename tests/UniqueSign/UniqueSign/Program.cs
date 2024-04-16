14a13f4f769698051735c0fb07184124e961b1d6
Kate <mikhalevich%katya.com>
2024-04-16 13:14:01 +0300

    2 метода

--- tests/UniqueSign/UniqueSign/Program.cs
+++ tests/UniqueSign/UniqueSign/Program.cs
@@ -1,34 +1,42 @@
-﻿Console.WriteLine("Введите строку");
+﻿using System;
+
+Console.WriteLine("Введите строку");
 string? str = Console.ReadLine();
 
 if (str == null) Console.WriteLine("Введите строку");
 else
 {
+    // убрать, если нужно считать пробелы 
     str = str.Replace(" ", string.Empty);
 
     Console.WriteLine($"Длинна {str.Length}");
 
-
-    void numbersInString (string str, int count)
+    int count = 0;
+    void numbersInString (string str)
     {
         for (int index1 = 0; index1 < str.Length; index1++)
         {
-        int temp = 0;
-            for (int index2 = 0; index2 < str.Length; index2++)
-            {
-                if (str[index1] == str[index2])
-                {
-                    temp++;
-                }
-            }
-            if (temp == 1)
-            {
-                Console.WriteLine($"Уникальный символ {str[index1]}");
-                count++;
-            }
+            numb(0, index1);
         }
         Console.WriteLine($"Всего {count} уникальных символов");
     }
 
-    numbersInString(str, 0);
+    int numb(int temp, int index1)
+    {
+        for (int index2 = 0; index2 < str.Length; index2++)
+        {
+            if (str[index1] == str[index2])
+            {
+                temp++;
+            }
+        }
+        if (temp == 1)
+        {
+            Console.WriteLine($"Уникальный символ {str[index1]}");
+            count++;
+        }
+        return count;
+    }
+
+    numbersInString(str);
 }
