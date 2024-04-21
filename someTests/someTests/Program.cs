59c1b3d971b0d538f881e93bcf1199bc8b2fc963
Kate <mikhalevich%katya.com>
2024-04-21 20:29:36 +0300

    All works (puzzle)

--- someTests/someTests/Program.cs
+++ someTests/someTests/Program.cs
@@ -149,7 +149,7 @@ Console.WriteLine(t);
 */
 
 //Фибаначи 
-
+/*
 int Fibonachi(int n)
 {
     if (n == 0 || n == 1) return n;
@@ -159,12 +159,12 @@ int Fibonachi(int n)
 
 int fib4 = Fibonachi(4);
 int fib5 = Fibonachi(5);
-int fib6 = Fibonachi(7);
+int fib6 = Fibonachi(6);
 
 Console.WriteLine($"4 число Фибоначчи = {fib4}");
 Console.WriteLine($"5 число Фибоначчи = {fib5}");
 Console.WriteLine($"6 число Фибоначчи = {fib6}");
-
+*/
 
 
 // Enum
