c16c8130a034e41e4e51dc6db469749534b2a67c
Kate <109430305+xLastNightx@users.noreply.github.com>
2024-05-09 18:59:25 +0300

    Delete someTests directory

--- someTests/someTests/GlobalUsing.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-global using someTests; // со словом global онон подрубается во все файлы проекта
