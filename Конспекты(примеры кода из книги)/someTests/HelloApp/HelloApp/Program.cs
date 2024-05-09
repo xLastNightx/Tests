211126c133f772329afdfafe204e38ec27e312d5
Kate <109430305+xLastNightx@users.noreply.github.com>
2024-05-09 19:48:33 +0300

    Add files via upload

---
+++ Конспекты(примеры кода из книги)/someTests/HelloApp/HelloApp/Program.cs
@@ -0,0 +1,7 @@
+﻿{
+    Console.WriteLine("Привет");
+    Console.WriteLine("Добро пожаловать в C#");
+}
+
+var name = Console.ReadLine();       // вводим имя
+Console.WriteLine($"Привет {name}");    // выводим имя на консоль                        
