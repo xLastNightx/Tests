df0a3c0abccc668fb81664b60b97a28326880760
Kate <109430305+xLastNightx@users.noreply.github.com>
2024-05-09 19:15:32 +0300

    Add files via upload

---
+++ Tests/Git/puzzle/puzzle/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите строку");
+string? str = Console.ReadLine();
+
+if (str == null) Console.WriteLine("Введите строку");
+else
+{
+    doubleWord(str);
+}
+
+static bool doubleWord (string str)
+{
+    int temp = 0;
+    for (int index1 = 0; index1 < str.Length - 1; index1++)
+    {
+            if (str[index1] == str[index1 + 1]) temp++;
+    }
+    if (temp > 0)
+    {
+        Console.WriteLine("Yes");
+        return true; 
+    }
+    else return false;
+}
