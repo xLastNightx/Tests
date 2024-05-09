f15516068afe1cbd26d615eef3871559175c69cf
Kate <109430305+xLastNightx@users.noreply.github.com>
2024-05-09 19:38:08 +0300

    Add files via upload

---
+++ Tests/ReverseAllWordsInString/ReverseAllWordsInString/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите предложение");
+string? str = Console.ReadLine();
+
+if (str != null) Console.WriteLine(reverseWordsInString(str));
+else Console.WriteLine("Неверное значение");
+
+static string reverseWordsInString(string stringFromConsole)
+{
+    string[] massivWithWords = stringFromConsole.Split(' ');
+    string readyString = "";
+
+    for (int i = 0; i < massivWithWords.Length; i++)
+    {
+        char[] charArray = massivWithWords[i].ToCharArray();
+        Array.Reverse(charArray);
+
+        readyString += new string(charArray);
+        readyString += " ";
+    }
+    readyString = readyString.Trim();
+
+    return readyString;
+}
+
